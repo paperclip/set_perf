69a2e7ae076b319698dd460e2da837323f024dde
Douglas Leeder <douglas.leeder@gmail.com>
2026-05-07 17:50:08 +0100

    Add boost flat_set. Move more code to shared functions.

--- benchmark.cpp
+++ benchmark.cpp
@@ -9,6 +9,7 @@
 #endif
 
 #include <boost/dynamic_bitset/dynamic_bitset.hpp>
+#include <boost/container/flat_set.hpp>
 
 #include <algorithm>
 #include <bitset>
@@ -28,35 +29,64 @@ using VectorType = std::vector<uint32_t>;
 
 #define PRINT(x) std::cerr << x << '\n'
 
-static VectorType dedup_boost_dynamic_bitset(const VectorType& input, unsigned maxValue)
+template<class SetType>
+static void dedup_generic_set_helper(const VectorType& input, VectorType out, SetType& seen)
 {
-    boost::dynamic_bitset<> seen{ maxValue, 0 };
-    seen.reset();
-    VectorType out;
+    for (const auto& x : input) 
+    {
+        if (seen.insert(x).second)
+        {
+            out.push_back(x);
+        }
+    }
+}
 
-    for (uint32_t x : input) 
+template<class BitSetType>
+static void dedup_generic_bitset_helper(const VectorType& input, VectorType out, BitSetType& seen)
+{
+    for (const auto& x : input) 
     {
         if (!seen.test(x)) {
             seen.set(x);
             out.push_back(x);
         }
     }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+static VectorType dedup_boost_dynamic_bitset(const VectorType& input, unsigned maxValue)
+{
+    boost::dynamic_bitset<> seen{ maxValue, 0 };
+    seen.reset();
+    VectorType out;
+    dedup_generic_bitset_helper(input, out, seen);
     return out;
 }
 
+static VectorType dedup_boost_flat_set(const VectorType& input)
+{
+    boost::container::flat_set<uint32_t> seen;
+    VectorType out;
+    dedup_generic_set_helper(input, out, seen);
+    return out;
+}
+
+static VectorType dedup_boost_flat_set_reserve(const VectorType& input, unsigned maxValue)
+{
+    boost::container::flat_set<uint32_t> seen;
+    seen.reserve(maxValue);
+    VectorType out;
+    out.reserve(input.size());
+    dedup_generic_set_helper(input, out, seen);
+    return out;
+}
 template<std::size_t MAX_VALUE>
 static VectorType dedup_std_bitset_heap(const VectorType& input)
 {
     std::unique_ptr<std::bitset<MAX_VALUE>> seen = std::make_unique<std::bitset<MAX_VALUE>>();
     VectorType out;
-
-    for (uint32_t x : input) 
-    {
-        if (!seen->test(x)) {
-            seen->set(x);
-            out.push_back(x);
-        }
-    }
+    dedup_generic_bitset_helper(input, out, *seen);
     return out;
 }
 
@@ -66,14 +96,7 @@ static VectorType dedup_std_bitset_stack(const VectorType& input)
     static_assert(MAX_VALUE <= 1'000'000, "MAX_VALUE is too large for stack allocation");
     std::bitset<MAX_VALUE> seen;
     VectorType out;
-
-    for (uint32_t x : input) 
-    {
-        if (!seen.test(x)) {
-            seen.set(x);
-            out.push_back(x);
-        }
-    }
+    dedup_generic_bitset_helper(input, out, seen);
     return out;
 }
 
@@ -81,25 +104,16 @@ static VectorType dedup_unordered_set(const VectorType& input)
 {
     std::unordered_set<uint32_t> seen;
     VectorType out;
-
-    for (const auto& x : input) 
-    {
-        if (seen.insert(x).second) out.push_back(x);
-    }
+    dedup_generic_set_helper(input, out, seen);
     return out;
 }
 
 static VectorType dedup_unordered_set_reserve(const VectorType& input)
 {
     std::unordered_set<uint32_t> seen;
-    seen.reserve(input.size());
     VectorType out;
     out.reserve(input.size());
-
-    for (const auto& x : input) 
-    {
-        if (seen.insert(x).second) out.push_back(x);
-    }
+    dedup_generic_set_helper(input, out, seen);
     return out;
 }
 
@@ -128,17 +142,9 @@ static VectorType dedup_plf_bitset_stack(const VectorType& input)
     static_assert(MAX_VALUE <= 1'000'000, "MAX_VALUE is too large for stack allocation");
     plf::bitset<MAX_VALUE> seen;
     VectorType out;
-    for (uint32_t x : input) 
-    {
-        if (!seen.test(x))
-        {
-            seen.set(x);
-            out.push_back(x);
-        }
-    }
+    dedup_generic_bitset_helper(input, out, seen);
     return out;
 }
-
 
 template<std::size_t MAX_VALUE>
 static VectorType dedup_plf_bitset_heap(const VectorType& input)
@@ -146,14 +152,7 @@ static VectorType dedup_plf_bitset_heap(const VectorType& input)
     using bitset = plf::bitset<MAX_VALUE>;
     std::unique_ptr<bitset> seen = std::make_unique<bitset>();
     VectorType out;
-
-    for (uint32_t x : input) 
-    {
-        if (!seen->test(x)) {
-            seen->set(x);
-            out.push_back(x);
-        }
-    }
+    dedup_generic_bitset_helper(input, out, *seen);
     return out;
 }
 
@@ -163,11 +162,7 @@ static VectorType dedup_set(const VectorType& input)
 {
     std::set<uint32_t> seen;
     VectorType out;
-
-    for (uint32_t x : input) 
-    {
-        if (seen.insert(x).second) out.push_back(x);
-    }
+    dedup_generic_set_helper(input, out, seen);
     return out;
 }
 
@@ -178,11 +173,7 @@ static VectorType dedup_flat_set(const VectorType& input)
 {
     std::flat_set<uint32_t> seen;
     VectorType out;
-
-    for (const auto& x : input) 
-    {
-        if (seen.insert(x).second) out.push_back(x);
-    }
+    dedup_generic_set_helper(input, out, seen);
     return out;
 }
 #endif /* FLAT_SET_AVAILABLE */
@@ -221,18 +212,25 @@ static void runTest(int minEpochIterations = 10)
     bench.relative(true);
     bench.minEpochIterations(minEpochIterations);
 
-    bench.run("unordered_set", [&] {
+    bench.run("std::unordered_set", [&] {
         ankerl::nanobench::doNotOptimizeAway(dedup_unordered_set(input));
     });
-    bench.run("unordered_set_reserve", [&] {
+    bench.run("std::unordered_set reserve", [&] {
         ankerl::nanobench::doNotOptimizeAway(dedup_unordered_set_reserve(input));
     });
-    bench.run("set", [&] {
+    bench.run("std::set", [&] {
         ankerl::nanobench::doNotOptimizeAway(dedup_set(input));
     });
-    bench.run("boost dynamic bitset", [&] {
+    bench.run("boost::dynamic_bitset", [&] {
         ankerl::nanobench::doNotOptimizeAway(dedup_boost_dynamic_bitset(input, maxValue));
     });
+    bench.run("boost::flat_set", [&] {
+        ankerl::nanobench::doNotOptimizeAway(dedup_boost_flat_set(input));
+    });
+    bench.run("boost::flat_set reserve", [&] {
+        ankerl::nanobench::doNotOptimizeAway(dedup_boost_flat_set_reserve(input, maxValue));
+    });
+
     bench.run("std::bitset heap", [&] {
         ankerl::nanobench::doNotOptimizeAway(dedup_std_bitset_heap<maxValue>(input));
     });
@@ -253,26 +251,26 @@ static void runTest(int minEpochIterations = 10)
         });
 #endif
     }
-    bench.run("sort_unique", [&] {
+    bench.run("sort unique", [&] {
         ankerl::nanobench::doNotOptimizeAway(dedup_sort_unique(input));
     });
-    bench.run("ranges_sort_unique", [&] {
+    bench.run("ranges sort unique", [&] {
         ankerl::nanobench::doNotOptimizeAway(dedup_ranges_sort_unique(input));
     });
 
-    #ifdef FLAT_SET_AVAILABLE
+#ifdef FLAT_SET_AVAILABLE
     bench.run("std::flat_set", [&] {
         ankerl::nanobench::doNotOptimizeAway(dedup_flat_set(input));
     });
-    #endif
+#endif
     
 }
 
 template<std::size_t n>
-static void runTest()
+static void runTest(int minEpochIterations = 10)
 {
     constexpr auto maxValue = n*2;
-    runTest<n, maxValue>();
+    runTest<n, maxValue>(minEpochIterations);
 }
 
 int main()
@@ -282,7 +280,7 @@ int main()
     runTest<2048>();
     runTest<32768>();
     runTest<131072>();
-    runTest<500000>();
+    runTest<500000>(2);
 
     // Sparse inputs
     runTest<8, 100'000'000>(100);
@@ -290,7 +288,7 @@ int main()
     runTest<2048, 100'000'000>(100);
     runTest<8192, 100'000'000>(100);
     runTest<32768, 100'000'000>(100);
-    runTest<65536, 100'000'000>(100);
+    runTest<65536, 100'000'000>(10);
 
     // oversubscribed inputs
     runTest<2048, 128>();
